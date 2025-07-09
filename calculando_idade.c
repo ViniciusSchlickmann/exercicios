69e467f73fdbb75245a2fdd86da7ab489a852082
Vinícius Willig Schlickmann <vwschlickmann@gmail.com>
2025-07-09 12:16:29 -0300

    subindo meus exercicios em linguagem C

---
+++ calculando_idade.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int ano_nasc, idade;
+	printf ("Qual ano do seu nascimento?");
+	scanf("%d", &ano_nasc);
+	idade = (2025 - ano_nasc);
+}
---
+++ somando_numeros.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int numero1, numero2, soma;
+	printf ("Qual primeiro numero?");
+	scanf("%d", &numero1);
+	
+	printf ("Qual segundo numero?");
+	scanf("%d", &numero2);
+	
+	soma = (numero1 - numero2);
+}
